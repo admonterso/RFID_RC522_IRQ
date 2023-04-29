c73179e49a27b5361fff12bc8565b088c0c3f066
Kaxa Gvimradze <kaxagvimraze@gmail.com>
2023-04-29 03:56:45 -0700

    arduino nano interrupt rc522

---
+++ ardInterruptRC522.ino
@@ -0,0 +1,103 @@
+/**
+
+ * Typical pin layout used:
+ * -----------------------------------------------------------------------------------------
+ *             MFRC522      Arduino       Arduino   Arduino    Arduino          Arduino
+ *             Reader/PCD   Uno/101       Mega      Nano v3    Leonardo/Micro   Pro Micro
+ * Signal      Pin          Pin           Pin       Pin        Pin              Pin
+ * -----------------------------------------------------------------------------------------
+ * RST/Reset   RST          9             5         D9         RESET/ICSP-5     RST
+ * SPI SS      SDA(SS)      10            53        D10        3                10
+ * IRQ         ?            ?             ?         ?          2                10
+ * SPI MOSI    MOSI         11 / ICSP-4   51        D11        ICSP-4           16
+ * SPI MISO    MISO         12 / ICSP-1   50        D12        ICSP-1           14
+ * SPI SCK     SCK          13 / ICSP-3   52        D13        ICSP-3           15
+  * 
+ */
+
+#include <SPI.h>
+#include <MFRC522.h>
+
+#define RST_PIN         9           // Configurable, see typical pin layout above
+#define SS_PIN          10           // Configurable, see typical pin layout above
+#define IRQ_PIN         2           // Configurable, depends on hardware
+
+MFRC522 mfrc522(SS_PIN, RST_PIN);   // Create MFRC522 instance.
+
+MFRC522::MIFARE_Key key;
+
+volatile bool bNewInt = false;
+byte regVal = 0x7F;
+void activateRec(MFRC522 mfrc522);
+void clearInt(MFRC522 mfrc522);
+
+/**
+ * Initialize.
+ */
+void setup() {
+  Serial.begin(115200); // Initialize serial communications with the PC
+  while (!Serial);      // Do nothing if no serial port is opened (added for Arduinos based on ATMEGA32U4)
+  SPI.begin();          // Init SPI bus
+
+  mfrc522.PCD_Init(); // Init MFRC522 card
+  mfrc522.PCD_DumpVersionToSerial(); 
+  /* read and printout the MFRC522 version (valid values 0x91 & 0x92)*/
+  
+
+  /* setup the IRQ pin*/
+  pinMode(IRQ_PIN, INPUT_PULLUP);
+
+  /*
+   * Allow the ... irq to be propagated to the IRQ pin
+   * For test purposes propagate the IdleIrq and loAlert
+   */
+  regVal = 0xA0; //rx irq
+  mfrc522.PCD_WriteRegister(mfrc522.ComIEnReg, regVal);
+ 
+  bNewInt = false; //interrupt flag
+
+  /*Activate the interrupt*/
+  attachInterrupt(digitalPinToInterrupt(IRQ_PIN), readCard, FALLING);
+
+  do { //clear a spourious interrupt at start
+    ;
+  } while (!bNewInt);
+  bNewInt = false;
+
+  Serial.println(F("End setup"));
+}
+
+/**
+ * Main loop.
+ */
+void loop() {
+ 
+
+  //activateRec(mfrc522);
+} //loop()
+
+
+
+void readCard() {
+    // Show some details of the PICC (that is: the tag/card)
+    // Reset the loop if no new card present on the sensor/reader. This saves the entire process when idle.
+            if ( ! mfrc522.PICC_IsNewCardPresent()) {
+                        return;
+            }
+            // Select one of the cards
+            if ( ! mfrc522.PICC_ReadCardSerial()) {
+                        return;
+            }
+            // Dump debug info about the card; PICC_HaltA() is automatically called
+            mfrc522.PICC_DumpToSerial(&(mfrc522.uid));
+    clearInt(mfrc522);
+}
+
+
+
+/*
+ * The function to clear the pending interrupt bits after interrupt serving routine
+ */
+void clearInt(MFRC522 mfrc522) {
+  mfrc522.PCD_WriteRegister(mfrc522.ComIrqReg, 0x7F);
+}
